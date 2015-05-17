f7ac62a2c57507842328cd3bfd247ecdbe017417
liangguoqiang <22894603@qq.com>
2015-05-17 15:30:38 +0800

    appAatch

---
+++ ASmallUniverseWatchApp/ASmallUniverseWatchApp WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  ASmallUniverseWatchApp WatchKit Extension
+//
+//  Created by apple on 15/5/16.
+//  Copyright (c) 2015年 apple. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
---
+++ ASmallUniverseWatchApp/ASmallUniverseWatchApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ASmallUniverseWatchApp
+//
+//  Created by apple on 15/5/16.
+//  Copyright (c) 2015年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
