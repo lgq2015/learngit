02e083e2fa57b7f5e9d40a6aa5eb76fbeb1eeef7
liangguoqiang <22894603@qq.com>
2015-05-17 17:10:51 +0800

    add

---
+++ cosmosWatch/cosmosWatch WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  cosmosWatch WatchKit Extension
+//
+//  Created by apple on 15/5/17.
+//  Copyright (c) 2015年 apple. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
---
+++ cosmosWatch/cosmosWatch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cosmosWatch
+//
+//  Created by apple on 15/5/17.
+//  Copyright (c) 2015年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
